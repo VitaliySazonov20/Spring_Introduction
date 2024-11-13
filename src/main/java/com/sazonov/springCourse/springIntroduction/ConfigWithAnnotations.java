aa49f59da93baae640b368baf66a59aae58445ef
V.Sazonov <VitaliySazonov20@gmail.com>
2024-11-13 12:49:38 +0400

    Annotation autowired for constructors

--- src/main/java/com/sazonov/springCourse/springIntroduction/ConfigWithAnnotations.java
+++ src/main/java/com/sazonov/springCourse/springIntroduction/ConfigWithAnnotations.java
@@ -6,7 +6,9 @@ public class ConfigWithAnnotations {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext3.xml");
-        Cat myCat = context.getBean("catBean", Cat.class);
+//        Cat myCat = context.getBean("catBean", Cat.class);
+        Person person =context.getBean("personBean", Person.class);
+        person.callYourPet();
         context.close();
     }
 }
--- src/main/java/com/sazonov/springCourse/springIntroduction/Person.java
+++ src/main/java/com/sazonov/springCourse/springIntroduction/Person.java
@@ -1,8 +1,15 @@
 package com.sazonov.springCourse.springIntroduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
     private Pet pet;
     private int age;
+
+    /*Autowired annotation automatically uses needed Beans in constructor*/
+    @Autowired
     public  Person(Pet pet){
         System.out.println("Person Bean created.");
         this.pet = pet;
