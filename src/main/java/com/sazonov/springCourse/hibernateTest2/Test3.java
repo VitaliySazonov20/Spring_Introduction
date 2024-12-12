1df6fac8becca2c7f36d145644843e6e57a7d39f
V.Sazonov <VitaliySazonov20@gmail.com>
2024-12-12 11:54:47 +0400

    OneToOne uni-directional part 2

---
+++ src/main/java/com/sazonov/springCourse/hibernateTest2/Test3.java
@@ -0,0 +1,31 @@
+package com.sazonov.springCourse.hibernateTest2;
+
+import com.sazonov.springCourse.hibernateTest2.entity.Details;
+import com.sazonov.springCourse.hibernateTest2.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test3 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Details.class)
+                .buildSessionFactory();
+        Session session = null;
+        try{// Session is the connection to the db
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            Employee employee = session.get(Employee.class, 4);
+            session.remove(employee);
+
+            session.getTransaction().commit();
+        }
+        finally {
+            assert session != null;
+            session.close();
+            factory.close();
+        }
+    }
+}
