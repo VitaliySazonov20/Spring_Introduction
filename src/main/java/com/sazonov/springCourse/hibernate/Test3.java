cb1024cd6b0a43b289b646441b74bae88e7529cb
V.Sazonov <VitaliySazonov20@gmail.com>
2024-12-12 09:51:19 +0400

    split test code into smaller tests

---
+++ src/main/java/com/sazonov/springCourse/hibernate/Test3.java
@@ -0,0 +1,43 @@
+package com.sazonov.springCourse.hibernate;
+
+import com.sazonov.springCourse.hibernate.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+
+public class Test3 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try{
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+//            Query is using Java classes/fields not name of tables or columns.
+            List<Employee> employeeList =session.createQuery("from Employee")
+                    .getResultList();
+
+            for (Employee emp: employeeList){
+                System.out.println(emp);
+            }
+
+            //surname is not the name of the column but the name of the field
+            //in Employee class.
+            List<Employee> employeeList2 = session.createQuery("from Employee "+
+                    "where surname = 'Doe'AND salary > 80_000")
+                    .getResultList();
+            for (Employee emp: employeeList2){
+                System.out.println(emp);
+            }
+        }
+        finally {
+            factory.close();
+        }
+    }
+}
